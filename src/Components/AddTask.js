8e079f6cc32f73569c8d5e5e652e35a2e7df664c
abdullah-mamun369 <maamamun369@gmail.com>
2023-04-27 17:00:33 +0600

    Add task field design is done

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
 import { AiOutlinePlus } from "react-icons/ai"
 import { BsThreeDotsVertical } from "react-icons/bs"
+import { TbSquareRounded } from "react-icons/tb"
+import { BiCheck } from "react-icons/bi"
 
 const AddTask = () => {
+    const [value, onChange] = useState(new Date());
+    const handleAddTask = () => {
+
+    }
+
+
     return (
         <div className='grid grid-cols-5 gap-5 mt-10 mx-10'>
-            <div className='bg-white p-4 rounded-lg drop-shadow-md'>
-                <div className='mb-6 flex justify-between'>
+            <div className='bg-white rounded-lg drop-shadow-md h-[300px]'>
+                <div className='mb-6 mx-4 mt-4 flex justify-between'>
                     <h4 className='text-[#7e104e] font-semibold text-xl'>My Tasks</h4>
 
                     <div className="dropdown dropdown-right">
@@ -16,10 +26,23 @@ const AddTask = () => {
                         </ul>
                     </div>
                 </div>
-                <button className='hover:text-[#7e104e] flex items-center'><span className='mr-2'><AiOutlinePlus /></span>Add a task</button>
+                <button onClick={handleAddTask} className='hover:text-[#7e104e] flex items-center mx-4 mb-2'><span className='mr-2'><AiOutlinePlus /></span>Add a task</button>
+                <div className='bg-slate-100 p-4'>
+                    <div className='flex justify-between'>
+                        <div>
+                            <input className='bg-slate-100 border-none outline-none' type="text" placeholder='Title' />
+                            <input className='bg-slate-100 border-none outline-none text-sm' type="text" placeholder='Details' />
+                        </div>
+                        <div>
+                            <BsThreeDotsVertical />
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
-            <div className='flex items-center'>
-                <button className='flex items-center text-sm font-semibold text-slate-400'><span className='mr-2'><AiOutlinePlus /></span>Add new list</button>
+            <div className=''>
+                <button className='flex items-center mt-5 text-sm font-semibold text-slate-400'><span className='mr-2'><AiOutlinePlus /></span>Add new list</button>
             </div>
         </div>
     );
